bc469de400bd950cfd3464d85cccb26a92eb1458
internet-cabbage <luthais.bradbury@gmail.com>
2026-09-26 12:02:49 +0200

    Formatting

--- macros.h
+++ macros.h
@@ -5,15 +5,15 @@
 #include "stdlib.h"
 #include <cuda_runtime.h>
 
-#define CUDA_CHECK(call)                                        \
-do {                                                            \
+#define CUDA_CHECK(call)                                            \
+do {                                                                \
     cudaError_t cudaErr = (call);                                   \
     if (cudaErr != cudaSuccess) {                                   \
-        printf("CUDA ERROR at: %s:%d\n",__FILE__,__LINE__);     \
+        printf("CUDA ERROR at: %s:%d\n",__FILE__,__LINE__);         \
         printf("%s\n",cudaGetErrorString(cudaErr));                 \
-        fflush(stdout);                                         \
-        exit(-1);                                               \
-    }                                                           \
+        fflush(stdout);                                             \
+        exit(-1);                                                   \
+    }                                                               \
 } while (0)
 
 #endif
